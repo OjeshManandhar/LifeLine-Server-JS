0f0905afaf9a41bfa5bf76edda22f60fd57b2c42
OjeshManandhar <ozes.manandhar@gmail.com>
2021-02-25 19:38:30 +0545

    Create a basic server

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hello World!!');
+// packages
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// env
+require('dotenv').config();
+
+const app = express();
+
+// Request parser
+app.use(bodyParser.json());
+
+app.use((req, res, next) => res.send(req.url));
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log('Server started at port:', port));
