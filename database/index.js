d35dc7e04811f6f7ada1d809dd13f0148591e80a
OjeshManandhar <ozes.manandhar@gmail.com>
2021-02-26 09:39:28 +0545

    Write DB connection code

---
+++ database/index.js
@@ -0,0 +1,23 @@
+const { DataTypes, Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    dialect: process.env.DB_DIALECT,
+    host: process.env.DB_HOST
+  }
+);
+
+// Test connection
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
+
+module.exports = { sequelize, DataTypes };
