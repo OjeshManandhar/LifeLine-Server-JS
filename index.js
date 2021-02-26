cecc64e66f301fb106aed64745a341be34e1fa36
OjeshManandhar <ozes.manandhar@gmail.com>
2021-02-26 23:01:09 +0545

    Use traffic routes in index

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const initModels = require('./database/initModels');
 
 // routers
 const driverRouter = require('./routes/driver');
+const trafficRoutes = require('./routes/traffic');
 
 const app = express();
 
@@ -27,7 +28,9 @@ app.use((req, res, next) => {
 
 // Routers
 app.use(driverRouter);
+app.use(trafficRoutes);
 
+// 404
 app.use((req, res, next) => res.status(404).json({ err: 'Path not found' }));
 
 // Error handling middleware
