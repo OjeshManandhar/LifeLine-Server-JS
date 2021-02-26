571a37406adbba6d28a2e04c57e3ae53508f6ffe
OjeshManandhar <ozes.manandhar@gmail.com>
2021-02-26 10:41:16 +0545

    Add CORS headers

--- index.js
+++ index.js
@@ -9,10 +9,24 @@ const bodyParser = require('body-parser');
 const { sequelize } = require('./database');
 const initModels = require('./database/initModels');
 
+// routers
+const driverRouter = require('./routes/driver');
+
 const app = express();
 
 // Request parser
 app.use(bodyParser.json());
+
+// CORS headers
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+// Routers
+app.use(driverRouter);
 
 app.use((req, res, next) => res.send(req.url));
 
